5b3af82fd03cc2eea2b17f2fb49b8e72cdc3c35b
Vany005 <vaniabeatriz999@gmail.com>
2024-06-10 11:39:59 -0300

    Parte um do projeto

---
+++ src/main/java/com/generation/Model/Categoria.java
@@ -0,0 +1,62 @@
+package com.generation.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Nome é obrigatório!")
+	@Size(min = 10, max = 500, message = "O atributo Nome deve ter no mínimo 1o caracteres e no máximo 500 caracteres.")
+	private String nome;
+	
+	
+	@NotBlank(message = "O campo descrição não pode estar vazio")
+	@Size(min = 10, max = 1000, message = "O campo descrição precisa ter entre 10 a 1000 caracteres")
+	private String descricao;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+
+
+}
